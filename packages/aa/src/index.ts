4992a9a7fdd24e0edf9f71b10c7ba5693f39afc9
AutoWallet Dev <noreply@autowallet.com>
2024-09-27 23:39:20 +0530

    chore: export gasEstimator and abis from AA package
    Make gas estimation utilities and ABI definitions available for import

--- packages/aa/src/index.ts
+++ packages/aa/src/index.ts
@@ -2,6 +2,8 @@ export * from './types.js';
 export * from './bundlerConfig.js';
 export * from './userOpBuilder.js';
 export * from './bundlerClient.js';
+export * from './gasEstimator.js';
 export * from './routeEncoder.js';
 export * from './routeUserOpBuilder.js';
 export * from './userOpHash.js';
+export * from './abis.js';
